f53e7a4a3349e6c27914d7ec8aecd15a6f38e016
matias <matiasbiasioli@gmail.com>
2024-05-04 10:21:38 -0300

    ending

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let mainDiv = document.querySelector('.mainDiv');
+let navbarToggler = document.querySelector('.navbar-toggler');
+let movePosition = false;
+
+navbarToggler.addEventListener('click', ()=>{
+    if (!movePosition){
+        mainDiv.classList.add('mainDivNavPosition');
+    } else {
+        mainDiv.classList.remove('mainDivNavPosition')
+    }
+    movePosition = !movePosition;
+})
+
+
+//Animations with AOS
 AOS.init();
+
+
